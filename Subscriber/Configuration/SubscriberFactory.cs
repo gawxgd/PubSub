3820c381b277ee6a704c39fd4eacb44cd5e5316c
Sandra Adamiec <sandra.adamiec.stud@pw.edu.pl>
2025-10-25 23:40:34 +0200

    bugfix

--- Subscriber/Configuration/SubscriberFactory.cs
+++ Subscriber/Configuration/SubscriberFactory.cs
@@ -10,7 +10,6 @@ namespace Subscriber.Configuration;
 
 public sealed class SubscriberFactory(ILogger logger) : ISubscriberFactory
 {
-    private static ILogger _logger;
     private const int MinPort = 1;
     private const int MaxPort = 65535;
     private const string AllowedUriScheme = "messageBroker";
@@ -19,7 +18,7 @@ public sealed class SubscriberFactory(ILogger logger) : ISubscriberFactory
     {
         var (host, port, topic, minLen, maxLen, poll, retry) = ValidateOptions(options);
         var channel = Channel.CreateUnbounded<byte[]>();
-        var connection = new TcpSubscriberConnection(host, port, channel.Writer, _logger);
+        var connection = new TcpSubscriberConnection(host, port, channel.Writer, logger);
 
         return new TcpSubscriber(
             topic,
@@ -33,32 +32,32 @@ public sealed class SubscriberFactory(ILogger logger) : ISubscriberFactory
 
     }
 
-    private static (string host, int port, string topic, int minLen, int maxLen, TimeSpan poll, uint retry)
+    private (string host, int port, string topic, int minLen, int maxLen, TimeSpan poll, uint retry)
         ValidateOptions(SubscriberOptions options)
     {
         var uri = options.MessageBrokerConnectionUri;
 
         if (!uri.IsAbsoluteUri)
         {
-            _logger.LogError(LogSource.Subscriber, $"{options.MessageBrokerConnectionUri} is not an absolute URI.");
+            logger.LogError(LogSource.Subscriber, $"{options.MessageBrokerConnectionUri} is not an absolute URI.");
             throw new SubscriberFactoryException("URI must be absolute", SubscriberFactoryErrorCode.InvalidUri);
         }
 
         if (!string.Equals(uri.Scheme, AllowedUriScheme, StringComparison.OrdinalIgnoreCase))
         {
-            _logger.LogError(LogSource.Subscriber, $"{options.MessageBrokerConnectionUri.Scheme} is not a valid scheme.");
+            logger.LogError(LogSource.Subscriber, $"{options.MessageBrokerConnectionUri.Scheme} is not a valid scheme.");
             throw new SubscriberFactoryException("Unsupported URI scheme", SubscriberFactoryErrorCode.UnsupportedScheme);
         }
 
         if (uri.Port is < MinPort or > MaxPort)
         {
-            _logger.LogError(LogSource.Subscriber, $"{options.Port} is not a valid port.");
+            logger.LogError(LogSource.Subscriber, $"{options.Port} is not a valid port.");
             throw new SubscriberFactoryException("Invalid port", SubscriberFactoryErrorCode.InvalidPort);
         }
 
         if (string.IsNullOrWhiteSpace(options.Topic))
         {
-            _logger.LogError(LogSource.Subscriber, $"{options.Topic} is not a topic.");
+            logger.LogError(LogSource.Subscriber, $"{options.Topic} is not a topic.");
             throw new SubscriberFactoryException("Topic is required", SubscriberFactoryErrorCode.MissingTopic);
         }
 
