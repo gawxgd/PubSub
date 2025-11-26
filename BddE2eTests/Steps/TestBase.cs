e1858833beea2770aef005cfbfaeeea117a23d05
Marcin Gronicki <mgronicki@box.com>
2025-11-26 12:59:52 +0100

    cc

---
+++ BddE2eTests/Steps/TestBase.cs
@@ -0,0 +1,18 @@
+using LoggerLib.Domain.Port;
+using LoggerLib.Outbound.Adapter;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace BddE2eTests.Steps;
+
+[SetUpFixture]
+public class TestBase
+{
+    [OneTimeSetUp]
+    public void InitializeLogger()
+    {
+        //ToDo should we initalize it like this?
+        var logger = Substitute.For<ILogger>();
+        AutoLoggerFactory.Initialize(logger);
+    }
+}
