0a02726bd87a14fa66fe1a4040bec89bbe3fb3cf
Sandra Adamiec <sandra.adamiec.stud@pw.edu.pl>
2026-01-07 11:59:25 +0100

    cleanup

--- Subscriber/src/Domain/ISubscriberConnection.cs
+++ Subscriber/src/Domain/ISubscriberConnection.cs
@@ -6,6 +6,4 @@ public interface ISubscriberConnection
 {
     Task ConnectAsync();
     Task DisconnectAsync();
-    //TODO consider place to put this metod as required?
-    //Task SendRequestAsync(string topic, ulong offset, CancellationToken cancellationToken = default);
 }
