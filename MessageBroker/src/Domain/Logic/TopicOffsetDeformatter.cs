0747788f142d134bc17efd6b8b5b95ac249e1906
Marcin Gronicki <mgronicki@box.com>
2026-01-09 22:39:07 +0100

    bug fix

--- MessageBroker/src/Domain/Logic/TopicOffsetDeformatter.cs
+++ MessageBroker/src/Domain/Logic/TopicOffsetDeformatter.cs
@@ -18,18 +18,17 @@ public class TopicOffsetDeformatter
 
         if (parts.Length != 2)
         {
-            Logger.LogWarning($"Invalid request format: {requestString}, using defaults");
+            Logger.LogWarning($"Invalid request format: {requestString}, returning null");
             return null;
         }
 
         var topic = parts[0];
         if (!ulong.TryParse(parts[1], out var offset))
         {
-            Logger.LogWarning($"Invalid offset in request: {parts[1]}, using 0");
-            offset = 0;
+            Logger.LogWarning($"Invalid offset in request: {parts[1]}, returning null");
+            return null;
         }
 
         return new TopicOffset(topic, offset);
     }
 }
-
